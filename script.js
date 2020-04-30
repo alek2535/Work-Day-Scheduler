a7ac45f9e5135d7ce73af21fec68f5eeeb711f40
Alek <alekvalencia2535@gmail.com>
2020-04-29 23:29:08 -0700

    added and fixed classes and ids for caling elements. changed icon size and padding around it. added date function, time function, and color block function. have a saveBtn event caller and a function for changing text for timeblock.

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
 $(document).ready(function() {
-    
-    $('.saveBtn').on('click', function () {
-        let userInput = $('.eventDescription');
-        let scheduleEvent = $('.text').text(userInput);
-        console.log(scheduleEvent);
-    })
+    //Moment Date
+    var currentDay = moment().format("dddd MMMM Do YYYY");
+        $("#currentDay").text(currentDay);
+    //Current Time for Present, Past, Future    
+    var currentHour = moment().format('H');
+    console.log(currentHour);
+
+    let userInput = '';
+
+    timeColor();
+
+    localStorage.getItem(JSON.stringify(userInput));
+
+//Timeblock Colors
+    function timeColor() {
+        if ($('.time').attr('value') === currentHour) {
+            $('.text-block').addClass('present');
+            } else if ($('.time').attr('value') > currentHour) {
+                $('.text-block').addClass('past');
+            } else if ($('.time').attr('value') < currentHour) {
+                $('.text-block').addClass('future');
+            }
+            console.log($('.time').attr('value'));
+    }
+
+    //Save Button
+    $('.saveBtn').on('click', changeText())
+
+    function changeText() {
+        userInput = $('#user-input-1').val();
+        $('#user-input-1').html(userInput);
+        localStorage.setItem(JSON.parse('userInput'));
+    }
+
 })
