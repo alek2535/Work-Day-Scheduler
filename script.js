d149bdef7247c67ac1c00de8c6986ef8d0d80951
Alek <alekvalencia2535@gmail.com>
2020-05-01 23:11:36 -0700

    fixed timeColor() to work correctly. tried to make code clean and non repetitive, but couldn't get everything to work. ended up creating local storage for each input as well as a save button feature as well.

--- script.js
+++ script.js
@@ -4,35 +4,82 @@ $(document).ready(function() {
         $("#currentDay").text(currentDay);
     //Current Time for Present, Past, Future    
     var currentHour = moment().format('H');
-    console.log(currentHour);
-
+    
+    //GetItem and display
+    $('#user-input-1').html(localStorage.getItem('user-input-1'));
+    $('#user-input-2').html(localStorage.getItem('user-input-2'));
+    $('#user-input-3').html(localStorage.getItem('user-input-3'));
+    $('#user-input-4').html(localStorage.getItem('user-input-4'));
+    $('#user-input-5').html(localStorage.getItem('user-input-5'));
+    $('#user-input-6').html(localStorage.getItem('user-input-6'));
+    $('#user-input-7').html(localStorage.getItem('user-input-7'));
+    $('#user-input-8').html(localStorage.getItem('user-input-8'));
+    $('#user-input-9').html(localStorage.getItem('user-input-9'));
 
     timeColor();
 
-    let userInputGet = localStorage.getItem('user input');
-    $('#user-input-1').html(userInputGet);
+
+    //$("#").append(localStorage.getItem(""));
 
 //Timeblock Colors
     function timeColor() {
-        if ($('.time').attr('value') === currentHour) {
-            $('.text-block').addClass('present');
-            } else if ($('.time').attr('value') > currentHour) {
-                $('.text-block').addClass('past');
-            } else if ($('.time').attr('value') < currentHour) {
-                $('.text-block').addClass('future');
-            }
-            console.log($('.time').attr('value'));
+        $('.text-block').each(function () {
+            let time = parseInt($(this).siblings().attr('value'));
+            if (time == currentHour) {
+                $(this).addClass('present');
+                } else if (time < currentHour) {
+                    $(this).addClass('past');
+                } else if (time > currentHour) {
+                    $(this).addClass('future');
+                }
+            })
     }
 
     //Save Button
     $('.saveBtn').on('click', function() {
-        let userInputSet = localStorage.setItem('user input', ($('#user-input-1').val()));
+        let userInputSet = localStorage.setItem('user-input-1', ($('#user-input-1').val()));
         $('#user-input-1').html(userInputSet);
     });
+    $('.saveBtn').on('click', function() {
+        let userInputSet = localStorage.setItem('user-input-2', ($('#user-input-2').val()));
+        $('#user-input-2').html(userInputSet);
+    });
+    $('.saveBtn').on('click', function() {
+        let userInputSet = localStorage.setItem('user-input-3', ($('#user-input-3').val()));
+        $('#user-input-3').html(userInputSet);
+    });
+    $('.saveBtn').on('click', function() {
+        let userInputSet = localStorage.setItem('user-input-4', ($('#user-input-4').val()));
+        $('#user-input-4').html(userInputSet);
+    });
+    $('.saveBtn').on('click', function() {
+        let userInputSet = localStorage.setItem('user-input-5', ($('#user-input-5').val()));
+        $('#user-input-5').html(userInputSet);
+    });
+    $('.saveBtn').on('click', function() {
+        let userInputSet = localStorage.setItem('user-input-6', ($('#user-input-6').val()));
+        $('#user-input-6').html(userInputSet);
+    });
+    $('.saveBtn').on('click', function() {
+        let userInputSet = localStorage.setItem('user-input-7', ($('#user-input-7').val()));
+        $('#user-input-7').html(userInputSet);
+    });
+    $('.saveBtn').on('click', function() {
+        let userInputSet = localStorage.setItem('user-input-8', ($('#user-input-8').val()));
+        $('#user-input-8').html(userInputSet);
+    });
+    $('.saveBtn').on('click', function() {
+        let userInputSet = localStorage.setItem('user-input-9', ($('#user-input-9').val()));
+        $('#user-input-9').html(userInputSet);
+    });
 
-    // function changeText() {
-        
-        // 
-    // };
+    // $('.saveBtn').on('click', function() {
+    //     let userText = $(this).siblings('.text-block').children('.text');
+    //     localStorage.setItem('user input', (userText.val()));
+    //     // $('#user-input-1').html(userInputSet);
+    //     console.log(userText);
+    // });
+
+
 
 });
